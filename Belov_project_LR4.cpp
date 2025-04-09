8d7c2a6d74c21b5531d123ae20165e17b415619a
Xcalib9r <oleg1470_df4@mail.ru>
2025-04-10 00:12:04 +0300

    change3

--- Belov_project_LR4.cpp
+++ Belov_project_LR4.cpp
@@ -17,7 +17,12 @@ void CalcSr()
 }
 void CalcGr()
 {
-
+    if (x <= 0 || y <= 0) {
+        cout << "Ошибка! Сначала введите X и Y.\n";
+        return;
+    }
+    double Gr = sqrt(x * y);
+    cout << "Среднее геометрическое: " << Gr << endl;
 }
 
 //структура меню
